476eb584bf22236eb35065aaa05ae0469a3908f3
smn-d <sday@outlook.fr>
2021-07-22 14:38:07 +0200

    up

--- sbm/script.py
+++ sbm/script.py
@@ -1,12 +1,12 @@
 import graph_tool.all as gt
 from sbm import sbm
 
-experimentName = "patientIcd"
+experimentName = "patientIcd10"
 
 model = sbm()
 
 # graph_title = "multi1000PatientDiseases.gt.gz"
-admin_num = 56115
+admin_num = 10
 model.make_graph(admin_num=admin_num, gender=False,ethnicity=False)
 
 model.save_graph(filename=experimentName+".gt.gz")
