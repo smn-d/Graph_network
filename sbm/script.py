9b858b5f0d39e6e9b1e3abad5d06243cc6d2ea49
smn-d <sday@outlook.fr>
2021-07-27 01:38:23 +0200

    ready

--- sbm/script.py
+++ sbm/script.py
@@ -4,41 +4,46 @@ from sbm import sbm
 experimentName = "test"
 
 model = sbm()
-patient_num = 115
-# patient_num = 56115
+# patient_num = 115
+patient_num = 56115
 model.make_graph(patient_num=patient_num, age=False,gender=False,ethnicity=False)
 experimentName = "56115nested_noDegCorr_noDem" 
 model.setOuputLoc(experimentName)
 model.fit(multilayer=False,deg_corr=False)
+print("experiment "+ experimentName+ " done")
 
 model = sbm()
-patient_num = 115
-# patient_num = 56115
+# patient_num = 115
+patient_num = 56115
 model.make_graph(patient_num=patient_num, age=False,gender=False,ethnicity=False)
 experimentName = "56115nested_DegCorr_noDem" 
 model.setOuputLoc(experimentName)
 model.fit(multilayer=False,deg_corr=True)
+print("experiment "+ experimentName+ " done")
 
 model = sbm()
-patient_num = 115
-# patient_num = 56115
+# patient_num = 115
+patient_num = 56115
 model.make_graph(patient_num=patient_num, age=True,gender=True,ethnicity=True)
 experimentName = "56115nested_DegCorr_Dem" 
 model.setOuputLoc(experimentName)
 model.fit(multilayer=False,deg_corr=True)
+print("experiment "+ experimentName+ " done")
 
 model = sbm()
-patient_num = 115
-# patient_num = 56115
+# patient_num = 115
+patient_num = 56115
 model.make_graph(patient_num=patient_num, age=True,gender=True,ethnicity=True)
 experimentName = "56115multi_noDegCorr_Dem" 
 model.setOuputLoc(experimentName)
 model.fit(multilayer=True,deg_corr=False)
+print("experiment "+ experimentName+ " done")
 
 model = sbm()
-patient_num = 115
-# patient_num = 56115
+# patient_num = 115
+patient_num = 56115
 model.make_graph(patient_num=patient_num, age=True,gender=True,ethnicity=True)
 experimentName = "56115mult_DegCorr_Dem" 
 model.setOuputLoc(experimentName)
 model.fit(multilayer=True,deg_corr=True)
+print("experiment "+ experimentName+ " done")
