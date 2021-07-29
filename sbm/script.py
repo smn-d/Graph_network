aa6b0f1c15fc45ee0217649771e6ddd75c0f2fc0
smn-d <sday@outlook.fr>
2021-07-29 09:37:26 +0200

    up

--- sbm/script.py
+++ sbm/script.py
@@ -8,10 +8,14 @@ model = sbm()
 # patient_num = 115
 patient_num = 56115
 # patient_num = 10000
-model.make_graph(patient_num=patient_num, age=False,gender=False,ethnicity=False)
+
 experimentName = str(patient_num)+"nested_noDegCorr_noDem" 
 # experimentName += "NoEq"
 model.setOuputLoc(experimentName)
+
+model.make_graph(patient_num=patient_num, age=False,gender=False,ethnicity=False)
+model.save_graph()
+
 model.fit(multilayer=False,deg_corr=False)
 print("experiment "+ experimentName+ " done")
 
