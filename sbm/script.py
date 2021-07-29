d5a801e01cdffed6104182659a43ab76107f4423
smn-d <sday@outlook.fr>
2021-07-29 12:40:55 +0200

    fix

--- sbm/script.py
+++ sbm/script.py
@@ -19,12 +19,15 @@ experimentName = "test"
 # model.fit(multilayer=False,deg_corr=False)
 # print("experiment "+ experimentName+ " done")
 
+
+folder = "data/"
+
 model = sbm()
 # patient_num = 115
 patient_num = 56115
 # patient_num = 10000
 model.make_graph(patient_num=patient_num, age=False,gender=False,ethnicity=False)
-experimentName = str(patient_num)+"nested_DegCorr_noDem" 
+experimentName = folder + str(patient_num)+"nested_DegCorr_noDem" 
 experimentName += "NoEq"
 model.setOuputLoc(experimentName)
 model.save_graph()
@@ -38,7 +41,7 @@ patient_num = 56115
 # patient_num = 10000
 model.make_graph(patient_num=patient_num, age=True,gender=True,ethnicity=True)
 
-experimentName = str(patient_num)+"nested_noDegCorr_Dem" 
+experimentName = folder + str(patient_num)+"nested_noDegCorr_Dem" 
 experimentName += "NoEq"
 
 model.setOuputLoc(experimentName)
@@ -53,7 +56,7 @@ patient_num = 56115
 # patient_num = 10000
 model.make_graph(patient_num=patient_num, age=True,gender=True,ethnicity=True)
 
-experimentName = str(patient_num)+"nested_DegCorr_Dem" 
+experimentName = folder + str(patient_num)+"nested_DegCorr_Dem" 
 experimentName += "NoEq"
 
 model.setOuputLoc(experimentName)
@@ -68,7 +71,7 @@ patient_num = 56115
 # patient_num = 10000
 model.make_graph(patient_num=patient_num, age=True,gender=True,ethnicity=True)
 
-experimentName = str(patient_num)+"multi_noDegCorr_Dem" 
+experimentName = folder + str(patient_num)+"multi_noDegCorr_Dem" 
 experimentName += "NoEq"
 
 model.setOuputLoc(experimentName)
@@ -84,7 +87,7 @@ patient_num = 56115
 
 model.make_graph(patient_num=patient_num, age=True,gender=True,ethnicity=True)
 
-experimentName = str(patient_num)+"mult_DegCorr_Dem" 
+experimentName = folder + str(patient_num)+"mult_DegCorr_Dem" 
 experimentName += "NoEq"
 
 model.setOuputLoc(experimentName)
