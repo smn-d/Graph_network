5c5a1d0d462758b1125fa1903784550998815447
smn-d <sday@outlook.fr>
2021-07-29 09:33:20 +0200

    up

--- sbm/script.py
+++ sbm/script.py
@@ -15,62 +15,62 @@ model.setOuputLoc(experimentName)
 model.fit(multilayer=False,deg_corr=False)
 print("experiment "+ experimentName+ " done")
 
-model = sbm()
-# patient_num = 115
-patient_num = 56115
-# patient_num = 10000
-model.make_graph(patient_num=patient_num, age=False,gender=False,ethnicity=False)
-experimentName = str(patient_num)+"nested_DegCorr_noDem" 
-# experimentName += "NoEq"
-model.setOuputLoc(experimentName)
-model.fit(multilayer=False,deg_corr=True)
-print("experiment "+ experimentName+ " done")
+# model = sbm()
+# # patient_num = 115
+# patient_num = 56115
+# # patient_num = 10000
+# model.make_graph(patient_num=patient_num, age=False,gender=False,ethnicity=False)
+# experimentName = str(patient_num)+"nested_DegCorr_noDem" 
+# # experimentName += "NoEq"
+# model.setOuputLoc(experimentName)
+# model.fit(multilayer=False,deg_corr=True)
+# print("experiment "+ experimentName+ " done")
 
-model = sbm()
-# patient_num = 115
-patient_num = 56115
-# patient_num = 10000
-model.make_graph(patient_num=patient_num, age=True,gender=True,ethnicity=True)
-experimentName = str(patient_num)+"nested_noDegCorr_Dem" 
-# experimentName += "NoEq"
+# model = sbm()
+# # patient_num = 115
+# patient_num = 56115
+# # patient_num = 10000
+# model.make_graph(patient_num=patient_num, age=True,gender=True,ethnicity=True)
+# experimentName = str(patient_num)+"nested_noDegCorr_Dem" 
+# # experimentName += "NoEq"
 
-model.setOuputLoc(experimentName)
-model.fit(multilayer=False,deg_corr=False)
-print("experiment "+ experimentName+ " done")
+# model.setOuputLoc(experimentName)
+# model.fit(multilayer=False,deg_corr=False)
+# print("experiment "+ experimentName+ " done")
 
-model = sbm()
-# patient_num = 115
-patient_num = 56115
-# patient_num = 10000
-model.make_graph(patient_num=patient_num, age=True,gender=True,ethnicity=True)
-experimentName = str(patient_num)+"nested_DegCorr_Dem" 
-# experimentName += "NoEq"
+# model = sbm()
+# # patient_num = 115
+# patient_num = 56115
+# # patient_num = 10000
+# model.make_graph(patient_num=patient_num, age=True,gender=True,ethnicity=True)
+# experimentName = str(patient_num)+"nested_DegCorr_Dem" 
+# # experimentName += "NoEq"
 
-model.setOuputLoc(experimentName)
-model.fit(multilayer=False,deg_corr=True)
-print("experiment "+ experimentName+ " done")
+# model.setOuputLoc(experimentName)
+# model.fit(multilayer=False,deg_corr=True)
+# print("experiment "+ experimentName+ " done")
 
-model = sbm()
-# patient_num = 115
-patient_num = 56115
-# patient_num = 10000
-model.make_graph(patient_num=patient_num, age=True,gender=True,ethnicity=True)
-experimentName = str(patient_num)+"multi_noDegCorr_Dem" 
-# experimentName += "NoEq"
+# model = sbm()
+# # patient_num = 115
+# patient_num = 56115
+# # patient_num = 10000
+# model.make_graph(patient_num=patient_num, age=True,gender=True,ethnicity=True)
+# experimentName = str(patient_num)+"multi_noDegCorr_Dem" 
+# # experimentName += "NoEq"
 
-model.setOuputLoc(experimentName)
-model.fit(multilayer=True,deg_corr=False)
-print("experiment "+ experimentName+ " done")
+# model.setOuputLoc(experimentName)
+# model.fit(multilayer=True,deg_corr=False)
+# print("experiment "+ experimentName+ " done")
 
-model = sbm()
-# patient_num = 115
-patient_num = 56115
-# patient_num = 10000
+# model = sbm()
+# # patient_num = 115
+# patient_num = 56115
+# # patient_num = 10000
 
-model.make_graph(patient_num=patient_num, age=True,gender=True,ethnicity=True)
-experimentName = str(patient_num)+"mult_DegCorr_Dem" 
-# experimentName += "NoEq"
+# model.make_graph(patient_num=patient_num, age=True,gender=True,ethnicity=True)
+# experimentName = str(patient_num)+"mult_DegCorr_Dem" 
+# # experimentName += "NoEq"
 
-model.setOuputLoc(experimentName)
-model.fit(multilayer=True,deg_corr=True)
-print("experiment "+ experimentName+ " done")
+# model.setOuputLoc(experimentName)
+# model.fit(multilayer=True,deg_corr=True)
+# print("experiment "+ experimentName+ " done")
